4e9ceb8ae5ce4f6ac3f8876cd846950d9a0e1a91
ozakiryota <kb22056@gmail.com>
2022-03-13 00:14:31 +0900

    Add a script.

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+xhost +
+
+image="rviz"
+tag="latest"
+
+docker run -it --rm \
+	-e "DISPLAY" \
+	-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	$image:$tag \
+	bash -c " \
+		roscore & \
+		rviz"
